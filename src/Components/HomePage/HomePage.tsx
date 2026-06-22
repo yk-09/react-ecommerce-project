b14bfd318b81ad7805371cef0d3937f35f9e3389
yk-09 <yk8688564@gmail.com>
2026-06-22 19:34:56 +0530

    Conditionally rendered hero section in hompage

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,26 +1,23 @@
-import './HomePage.css';
-import MySkeleton from '../SkeletonLoad/SkeletonLoad';
-import '../SkeletonLoad/Skeleton.css';
-import MyNavBarForLargeScreen from '../Navbar/LSNavBar';
-import NavBarSmallScreen, { SearchProducts } from '../Navbar/SSNavBar';
-import '../Navbar/NavBar.css';
-import { ProductsGrid } from './Products';
-import { useState } from 'react';
+import "./HomePage.css";
+import MySkeleton from "../SkeletonLoad/SkeletonLoad";
+import "../SkeletonLoad/Skeleton.css";
+import MyNavBarForLargeScreen from "../Navbar/LSNavBar";
+import NavBarSmallScreen, { SearchProducts } from "../Navbar/SSNavBar";
+import "../Navbar/NavBar.css";
+import { ProductsGrid } from "./Products";
+import { useState } from "react";
 import type { CartItem } from "../../types/cart";
-import { productsData } from '../../services/products-data';
-import type { Product } from '../../types/product';
+import { productsData } from "../../services/products-data";
+import type { Product } from "../../types/product";
 
 interface HomePagePros {
-  products: Product[],
-  isLoading: boolean,
-  cart: CartItem[],
-  setCart: React.Dispatch<
-    React.SetStateAction<CartItem[]>
-  >
+  products: Product[];
+  isLoading: boolean;
+  cart: CartItem[];
+  setCart: React.Dispatch<React.SetStateAction<CartItem[]>>;
 }
 
-export function HomePage({products, isLoading, cart, setCart}: HomePagePros){
-
+export function HomePage({ products, isLoading, cart, setCart }: HomePagePros) {
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
   const cartQuantity = cart.reduce(
@@ -28,9 +25,7 @@ export function HomePage({products, isLoading, cart, setCart}: HomePagePros){
     0
   );
 
-  function handleChange(
-    e: React.ChangeEvent<HTMLInputElement>
-  ) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const searchTerm = e.target.value.toLowerCase();
 
     if (searchTerm === "") {
@@ -40,22 +35,25 @@ export function HomePage({products, isLoading, cart, setCart}: HomePagePros){
 
     const newProducts = productsData.filter(
       (product, index) =>
-        product.name.toLowerCase().includes(searchTerm) &&
-        index < 10
+        product.name.toLowerCase().includes(searchTerm) && index < 10
     );
 
     setFilteredProducts(newProducts);
   }
   return (
-
     <>
       <title>Homepage</title>
 
-      <header style={{display: isLoading? 'none' : 'block'}}>
-        <MyNavBarForLargeScreen cartQuantity={cartQuantity} handleChange={handleChange} />
+      <header style={{ display: isLoading ? "none" : "block" }}>
+        <MyNavBarForLargeScreen
+          cartQuantity={cartQuantity}
+          handleChange={handleChange}
+        />
         <NavBarSmallScreen cartQuantity={cartQuantity} />
         <form className="search-products-sd" action="" method="get">
-          <label className="sr-only" htmlFor="search-bar">search products</label>
+          <label className="sr-only" htmlFor="search-bar">
+            search products
+          </label>
           <button type="submit">
             <svg
               className="search-icon"
@@ -73,27 +71,35 @@ export function HomePage({products, isLoading, cart, setCart}: HomePagePros){
               <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
             </svg>
           </button>
-          <input id="search-bar" name="search-bar" type="text" placeholder="Search your desires" onChange={handleChange} />
+          <input
+            id="search-bar"
+            name="search-bar"
+            type="text"
+            placeholder="Search your desires"
+            onChange={handleChange}
+          />
         </form>
-        {filteredProducts.length !== 0 && <SearchProducts filteredProducts={filteredProducts} />}
+        {filteredProducts.length !== 0 && (
+          <SearchProducts filteredProducts={filteredProducts} />
+        )}
       </header>
 
       <main>
-        {isLoading && <MySkeleton/>}
+        {isLoading && <MySkeleton />}
 
-        <section className="hero-container js-hero-section">
-          <h1 className="hero-main">
-            Jaha <span className="red-line">Paisa</span> khatam hota hai... <br />
-            <span className="green-underline">Par Kaamna nahi.</span>
-          </h1>
-          <p className="hero-italic">
-            Try karke dekhlo.
-          </p>
-        </section>
+        {!isLoading && (
+          <section className="hero-container js-hero-section">
+            <h1 className="hero-main">
+              Jaha <span className="red-line">Paisa</span> khatam hota hai...{" "}
+              <br />
+              <span className="green-underline">Par Kaamna nahi.</span>
+            </h1>
+            <p className="hero-italic">Try karke dekhlo.</p>
+          </section>
+        )}
 
         <ProductsGrid products={products} setCart={setCart} cart={cart} />
-        
       </main>
     </>
-  )
+  );
 }
